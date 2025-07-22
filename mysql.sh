c3c17ac87c3dd63cd6307a9f3d7c7dd16698cf71
Rushav chatraj <rushav182@gmail.com>
2025-07-22 10:45:34 +0530

    executing mysql logs

---
+++ mysql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%T)
+LOG_FILE_NAME="$LOG_FOLDER|$LOG_FILE-$TIMESTAMP.log"
+
+
+CHECK_ROOT()
+{
+    if [ id -ne 0 ]
+    then
+      echo "ERROR:: Sudo access is Required to execute the script"
+      exit 1
+    fi
+}
+
+echo "Script Started excecuted at: $TIMESTAMP" &>> $LOG_FILE_NAME
+
+CHECK_ROOT
